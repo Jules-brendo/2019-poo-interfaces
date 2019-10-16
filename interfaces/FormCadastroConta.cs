9b7cef8d802099d93bfadb41998fb858b1e48495
Jules-brendo <julesbreno@hotmail.com>
2019-10-16 14:21:42 -0300

    Add files via upload

---
+++ interfaces/FormCadastroConta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array
+{
+    public partial class FormCadastroConta : Form
+    {
+        private Form1 formPrincipal;
+
+        public FormCadastroConta(Form1 formPrincipal)
+        {
+            this.formPrincipal = formPrincipal;
+            InitializeComponent();
+        }
+        
+    private void BotaoCadastro_Click(object sender, EventArgs e)
+        {
+
+            if (comboTipoConta.Text == "Poupanca")
+                {
+                    ContaPoupanca novaConta = new ContaPoupanca();
+                    novaConta.Titular = new Cliente(textoTitular.Text);
+                     novaConta.Numero = Convert.ToInt32(textoNumero.Text);
+                 }
+             else
+                 {
+                     ContaCorrente novaConta = new ContaCorrente();
+                    novaConta.Titular = new Cliente(textoTitular.Text);
+                     novaConta.Numero = Convert.ToInt32(textoNumero.Text);
+                 }
+        }
+
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboTipoConta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void FormCadastroConta_Load(object sender, EventArgs e)
+        {
+            comboTipoConta.Items.Add("Poupança");
+            comboTipoConta.Items.Add("Corrente");
+        }
+    }
+    }
+
